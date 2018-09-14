080f1003a742ebfeed68670822b36051444f7fdc
DarushDev <darushfaqiri@gmail.com>
2018-09-14 13:18:34 +0800

    Animate using Object animator

--- app/src/main/java/com/example/sampleandroidanimationsandtransitions/MainActivity.java
+++ app/src/main/java/com/example/sampleandroidanimationsandtransitions/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sampleandroidanimationsandtransitions;
 
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvHello;
+    TextView tvTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvHello = findViewById(R.id.text_main_hello);
+        tvTwo = findViewById(R.id.text_main_text2);
 
         tvHello.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,11 +28,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        tvTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateUsingObjectAnimator();
+            }
+        });
+
 
     }
 
     private void animateTheText() {
-
         ValueAnimator animator = ValueAnimator.ofInt(0, getScreenWith() - tvHello.getWidth());
         animator.setDuration(1000);
         animator.start();
@@ -43,6 +52,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void animateUsingObjectAnimator() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(tvTwo, "translationY",
+                getScreenHeight() - getAbsoluteY(tvTwo) - tvTwo.getHeight());
+        animator.setDuration(1000);
+        animator.start();
+    }
+
     private int getScreenWith() {
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
@@ -53,6 +69,18 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         return metrics.heightPixels;
+    }
+
+    private int getAbsoluteX(View v) {
+        int[] location = new int[2];
+        v.getLocationOnScreen(location);
+        return location[0];
+    }
+
+    private int getAbsoluteY(View v) {
+        int[] location = new int[2];
+        v.getLocationOnScreen(location);
+        return location[1];
     }
 
 }

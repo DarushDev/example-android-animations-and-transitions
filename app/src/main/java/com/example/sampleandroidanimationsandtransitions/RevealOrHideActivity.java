379b70243bed1cd8f62197a646987cbfec5dbb54
DarushDev <darushfaqiri@gmail.com>
2018-09-17 13:24:29 +0800

    Circularly reveal or hide a view

--- app/src/main/java/com/example/sampleandroidanimationsandtransitions/RevealOrHideActivity.java
+++ app/src/main/java/com/example/sampleandroidanimationsandtransitions/RevealOrHideActivity.java
@@ -2,15 +2,19 @@ package com.example.sampleandroidanimationsandtransitions;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.TextView;
 
 public class RevealOrHideActivity extends AppCompatActivity {
 
     private View mContentView;
     private View mLoadingView;
     private int mAnimationDuration = 1500;
+    private TextView mTvLorem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,21 @@ public class RevealOrHideActivity extends AppCompatActivity {
 
         mContentView = findViewById(R.id.content);
         mLoadingView = findViewById(R.id.loading_spinner);
+        mTvLorem = findViewById(R.id.text_lorem);
+
+        findViewById(R.id.button_reveal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                circularReveal();
+            }
+        });
+
+        findViewById(R.id.button_hide).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                circularHide();
+            }
+        });
 
         //mAnimationDuration = getResources().getInteger(android.R.integer.config_longAnimTime);
     }
@@ -47,5 +66,46 @@ public class RevealOrHideActivity extends AppCompatActivity {
                         mLoadingView.setVisibility(View.GONE);
                     }
                 });
+    }
+
+    private void circularReveal() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // get the center for the clipping circle
+            int cx = mTvLorem.getWidth() / 2;
+            int cy = mTvLorem.getHeight() / 2;
+
+            //get the final radius for the clipping circle
+            float finalRadius = (float) Math.hypot(cx, cy);
+
+            Animator animator = ViewAnimationUtils.createCircularReveal(mTvLorem, cx, cy, 0, finalRadius);
+
+            mTvLorem.setVisibility(View.VISIBLE);
+            animator.start();
+        } else {
+            mTvLorem.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void circularHide() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            int cx = mTvLorem.getWidth() / 2;
+            int cy = mTvLorem.getHeight() / 2;
+
+            float initialRadius = (float) Math.hypot(cx, cy);
+
+            Animator animator = ViewAnimationUtils.createCircularReveal(mTvLorem, cx, cy, initialRadius, 0);
+
+            animator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    mTvLorem.setVisibility(View.INVISIBLE);
+                }
+            });
+
+            animator.start();
+        } else {
+            mTvLorem.setVisibility(View.VISIBLE);
+        }
     }
 }

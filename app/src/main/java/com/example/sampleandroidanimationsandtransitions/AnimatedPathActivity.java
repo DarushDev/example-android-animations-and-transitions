23df985447fa9090972af8e52aaca16374f0e84d
DarushDev <darushfaqiri@gmail.com>
2018-11-05 18:43:37 +0800

    Add an animated path view

--- app/src/main/java/com/example/sampleandroidanimationsandtransitions/AnimatedPathActivity.java
+++ app/src/main/java/com/example/sampleandroidanimationsandtransitions/AnimatedPathActivity.java
@@ -1,13 +1,23 @@
 package com.example.sampleandroidanimationsandtransitions;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 
 public class AnimatedPathActivity extends AppCompatActivity {
+
+    RelativeLayout parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animated_path);
+
+        /*parent = findViewById(R.id.layout_animated_path_parent);
+        HeartbeatView heartbeatView = new HeartbeatView(this);
+
+        parent.addView(heartbeatView);*/
+
+
     }
 }
